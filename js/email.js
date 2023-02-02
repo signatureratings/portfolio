e5a135c5ed093b14a5d75f60fa67ef981dbb2e3b
sairambalu <balusairam26@gmail.com>
2023-02-02 00:09:47 -0500

    first version portfolio

---
+++ js/email.js
@@ -0,0 +1,14 @@
+function sendEmail(x){
+    console.log(x)
+    // Email.send({
+    //     Host : "smtp.elasticemail.com",
+    //     Username : "contactsairambalu@gmail.com",
+    //     Password : "password",
+    //     To : 'them@website.com',
+    //     From : "you@isp.com",
+    //     Subject : "This is the subject",
+    //     Body : "And this is the body"
+    // }).then(
+    //   message => alert(message)
+    // );
+}
